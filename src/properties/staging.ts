3aec94d46c3d96afaf80ffbe1fef89247a38a211
Cameron Podd <podd@usc.edu>
2021-01-10 17:27:56 -0800

    Change to new azure app service

--- src/properties/staging.ts
+++ src/properties/staging.ts
@@ -9,14 +9,14 @@ const tenant = 'devwahinekaimemberdbauth.onmicrosoft.com';
 const applicationID = 'f1837521-8fa7-4b51-b860-ba1154a2e8d3';
 const tenantSubdomain = tenant.split('.')[0];
 const instance = `https://${tenantSubdomain}.b2clogin.com/tfp/`;
-const reactRedirectUri = 'https://development-wahinekaimemberdatabase.azurewebsites.net';
+const reactRedirectUri = 'https://dev-wahinekai-memberdatabase.azurewebsites.net';
 
 const signInPolicy = 'B2C_1_signup_signin_api';
 const signInAuthority = `${instance}${tenant}/${signInPolicy}`;
 const frontendAssetsPrefix = 'https://devmemberdatabasestorage.blob.core.windows.net/frontend-assets';
 
 const settings: Readonly<Settings> = {
-    backendEndpoint: 'https://backend-development-wahinekaimemberdatabase.azurewebsites.net/api/v1',
+    backendEndpoint: 'https://dev-api-wahinekai-memberdatabase.azurewebsites.net/api/v1',
     frontendAssetsPrefix,
     emptyProfileImage: `${frontendAssetsPrefix}/no-image.png`,
     ...globalSettings,
