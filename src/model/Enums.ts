2bd1bd16d3c8dafd9063266b60dd4b8eb7d654d8
keciamduffy <44991806+keciamduffy@users.noreply.github.com>
2025-03-26 10:20:45 -0700

    Update Enums.ts
    add corpus christi

--- src/model/Enums.ts
+++ src/model/Enums.ts
@@ -52,6 +52,7 @@ export enum Chapter {
     RockawayBeachNewYork = 'Rockaway Beach New York',
     WahineKaiInternational = 'Wahine Kai International',
     VirginiaBeach = 'Virginia Beach',
+    CorpusChristi = 'CorpusChristi',
 }
 
 /**
